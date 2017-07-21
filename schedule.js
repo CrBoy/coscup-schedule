6b3442bad65d248787d9271cbfa7d40ebc54bbf2
CrBoy <crboy@crboy.net>
2017-07-22 03:33:28 +0800

    .

--- schedule.js
+++ schedule.js
@@ -1,30 +1,32 @@
 "use strict"
 
-var vm = new Vue({
+let vm = new Vue({
 	el: "#schedule",
 	data: {
 		current_date: null,
 		current_event: null,
-		event_details_style: {top: 0, left: 0},
-		dates: [],
+		dates_ms: new Set(),
 		tag_list: [],
 		places: [],
 		events: {},
 		loaded: false
 	},
 	computed: {
-		tags: function(){
+		dates(){
+			return [...this.dates_ms].sort().map(ms => new Date(ms))
+		},
+		tags(){
 			return this.tag_list.filter(t => t.enabled).map(t => t.name)
 		},
-		hidden_places: function(){
+		hidden_places(){
 			return Object.keys(this.events).filter(p => this.places.indexOf(p) < 0)
 		},
-		visible_events: function(){
+		visible_events(){
 			return this.places.reduce((obj, p) => {
-				let events_here = this.events[p].map(e => {
+				const events_here = this.events[p].map(e => {
 					if (e.begin.toDateString() !== this.current_date.toDateString()) return null
 
-					let primary_tag = e.tags.find(t => this.tags.indexOf(t) >= 0)
+					const primary_tag = e.tags.find(t => this.tags.indexOf(t) >= 0)
 					if (primary_tag) {
 						return {
 							tags: e.tags,
@@ -33,7 +35,7 @@ var vm = new Vue({
 							owner: e.owner,
 							subject: e.subject,
 							description: e.description,
-							classes: ["type" + this.tags.indexOf(primary_tag)],
+							classes: ["type" + this.tag_list.findIndex(tag => tag.name === primary_tag)],
 							style: {
 								top: vm.px_in_col(e.begin) + 'px',
 								height: vm.px_in_col(e.end) - vm.px_in_col(e.begin) + 'px'
@@ -44,9 +46,9 @@ var vm = new Vue({
 					}
 				}).filter(e => e)
 
-				var chunks = [], tracks = [], latest = 0
-				for (var i in events_here) {
-					let e = events_here[i]
+				let chunks = [], tracks = [], latest = 0
+				for (let i in events_here) {
+					const e = events_here[i]
 
 					if (e.begin >= latest && tracks.length > 0) {
 						// new chunk
@@ -54,7 +56,7 @@ var vm = new Vue({
 						tracks = []
 					}
 
-					var track = tracks.find(t => e.begin >= t[t.length-1].end)
+					let track = tracks.find(t => e.begin >= t[t.length-1].end)
 					if (track) // add to track
 						track.push(e)
 					else // new track
@@ -65,7 +67,7 @@ var vm = new Vue({
 				chunks.push(tracks) // finalize
 
 				chunks.forEach(tracks => {
-					let w = 96/(tracks.length)
+					const w = 96/(tracks.length)
 					tracks.forEach((events, i) => {
 						events.forEach(e => {
 							e.style.width = w + '%'
@@ -80,33 +82,49 @@ var vm = new Vue({
 		}
 	},
 	methods: {
-		moment: moment,
-		px_in_col: function(time) {
-			let minutes = time.getHours() * 60 + time.getMinutes()
+		moment,
+		px_in_col(time) {
+			const minutes = time.getHours() * 60 + time.getMinutes()
 			return (100*24) * minutes / (60*24)
 		},
-		display_time: function(time) {
+		display_time(time) {
 			return moment(time).format("HH:mm")
 		},
-		show_event_details: function(e, $event) {
+		show_event_details(e, $event) {
 			this.current_event = e
-			if(e) this.event_details_style.top = e.style.top
 		},
-		resize_scroll: function(){
-			let scroll = document.getElementsByClassName("scroll-wrapper")[0]
+		resize_scroll(){
+			const scroll = document.getElementsByClassName("scroll-wrapper")[0]
 			scroll.style.height = (window.innerHeight - scroll.offsetTop - 2) + "px"
 		},
-		scroll_to_show: function(){
-			let scroll = document.getElementsByClassName("scroll-wrapper")[0]
+		scroll_to_show(){
+			const scroll = document.getElementsByClassName("scroll-wrapper")[0]
 			scroll.scrollTop = 850
-		}
+		},
+		add_events(events){
+			const dates_ms = events.map(e => moment(e.begin).startOf("day").valueOf())
+			this.add_dates(dates_ms)
+
+			events.reduce((obj, e) => {
+				if(typeof obj[e.place] == "undefined") this.$set(obj, e.place, [])
+				obj[e.place].push(e)
+				return obj
+			}, this.events)
+
+			Object.keys(this.events).forEach(p => {
+				this.events[p].sort((e1, e2) => e1.begin - e2.begin)
+			})
+		},
+		add_dates(dates_ms){
+			this.dates_ms = new Set([...this.dates_ms, ...dates_ms])
+		},
 	},
-	mounted: function(){
+	mounted(){
 		/*
 		axios.get("0804.json", {responseType: "json"}).then(function(res){
 			return // FIXME
-			var data = res.data
-			var events = data.places.reduce((obj, p) => {
+			let data = res.data
+			let events = data.places.reduce((obj, p) => {
 				obj[p] = []
 				return obj
 			}, {})
@@ -117,7 +135,7 @@ var vm = new Vue({
 				return obj
 			}, events)
 			Object.keys(events).forEach(function(p){
-				var es = events[p]
+				let es = events[p]
 				es.sort((e1, e2) => e1.begin - e2.begin)
 			})
 
@@ -129,37 +147,31 @@ var vm = new Vue({
 		*/
 
 		// load submissions
-		axios.get("https://coscup.org/2017-assets/json/submissions.json", {responseType: "json"}).then(function(res){
-			let data = res.data
-			let dates = data.map(e => moment(e.start).startOf("day").valueOf()).filter((date, i, dates) => dates.indexOf(date) === i).sort().map(ut => new Date(ut))
-			let events = data.reduce((obj, e) => {
-				if(typeof obj[e.room] == "undefined") obj[e.room] = []
-				obj[e.room].push({
-					tags: ["議程"],
-					place: e.room,
-					begin: new Date(e.start),
-					end: new Date(e.end),
-					owner: e.speaker.name,
-					subject: e.subject,
-					description: e.summary,
-					classes: [],
-				})
-				return obj
-			}, {})
-			Object.keys(events).forEach(function(p){
-				var es = events[p]
-				es.sort((e1, e2) => e1.begin - e2.begin)
-			})
+		axios.get("https://coscup.org/2017-assets/json/submissions.json", {responseType: "json"}).then(res => {
+			const data = res.data
 
-			vm.current_date = dates[0]
-			vm.dates = dates
-			vm.tag_list.push({name: "議程", enabled: true})
-			vm.places = Object.keys(events)
-			vm.events = events
-			vm.loaded = true
+			// get all dates
+			const events = data.map(e => ({
+				tags: ["議程"],
+				place: e.room,
+				begin: new Date(e.start),
+				end: new Date(e.end),
+				owner: e.speaker.name,
+				subject: e.subject,
+				description: e.summary,
+				classes: [],
+			}))
 
-			vm.resize_scroll()
-			vm.scroll_to_show()
+
+			this.add_events(events)
+			this.tag_list.push({name: "議程", enabled: true})
+			this.current_date = this.dates[0]
+			this.places = Object.keys(this.events)
+
+			this.loaded = true
+
+			this.resize_scroll()
+			this.scroll_to_show()
 		})
 	}
 })
